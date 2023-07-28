504b3a3f9a0f2274f6e5ff4e25b6e9e68fbabfb9
Ratigrace <chijongwegrace@gmail.com>
2023-07-28 15:05:49 -0700

    functions that print errors

--- errors2.c
+++ errors2.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * error_126 - Creates an error message for permission denied failures.
+ * error_101 - Creates an error message for permission denied failures.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *error_126(char **args)
+char *error_101(char **args)
 {
 	char *error, *hist_str;
 	int len;
@@ -35,12 +35,12 @@ char *error_126(char **args)
 }
 
 /**
- * error_127 - Creates an error message for command not found failures.
+ * error_102 - Creates an error message for command not found failures.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *error_127(char **args)
+char *error_102(char **args)
 {
 	char *error, *hist_str;
 	int len;
